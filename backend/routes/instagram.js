d096dfae2a806f63a5805909277f379a911415b9
Gus Vroustouris <gvrousto@purdue.edu>
2019-06-20 22:33:04 -0500

    finally authorizes correctly

--- backend/routes/instagram.js
+++ backend/routes/instagram.js
@@ -10,10 +10,37 @@ var login = (req, res) => {
 }
 
 var authorized = (req, res) => {
-  console.log(req.query['code']);
+  let code = req.query['code'];
+  tokenPromise(code).then((body)=>{
+    console.log(body);
+  });
   return res.redirect('http://localhost:3000/instaAuthorized');
 }
 
+let tokenPromise = (code) => {
+  let options = {
+    url: 'https://api.instagram.com/oauth/access_token',
+    form: {
+      client_id: config.client_id,
+      client_secret: config.client_secret,
+      grant_type: 'authorization_code',
+      redirect_uri: config.insta_redirect,
+      code: code,
+    }
+  };
+
+  console.log(options);
+
+  return new Promise( (resolve,reject) => {
+    request.post(options, function(err, resp, body) {
+      if(err){
+        reject(err);
+      }else{
+        resolve(body)
+      }
+    });
+  })
+}
 module.exports = {
   login: login,
   authorized: authorized
